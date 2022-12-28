76aadebbf43c588c442e602113d589be183c80c0
Mateus Sgubim <101438688+mateussgubim@users.noreply.github.com>
2022-12-28 11:01:46 -0300

    .

---
+++ exercicios/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do seu carro é de: ${vel} km/h`)
+if (vel > 60) {
+  console.log(`Você passou do limite permitido. MULTADO!`)
+}
+console.log(`Digija sempre usando cinto de segurança!`)
