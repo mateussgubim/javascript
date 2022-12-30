da4d8324df8e0a494111ac484084c61a27a7248a
Mateus Sgubim <101438688+mateussgubim@users.noreply.github.com>
2022-12-30 09:02:49 -0300

    .

--- exercicios/ex014/script.js
+++ exercicios/ex014/script.js
@@ -6,13 +6,16 @@ function carregar() {
   msg.innerHTML = `Agora são ${hora} horas.`
 
   if (hora >= 0 && hora < 12){
-    //BOM DIA
+    //BOM DIA 70bff8
     img.src = './images/foto-manha.png'
-  } else if(hora >= 12 && hora < 18) {
-    //BOA TARDE
+    document.body.style.background = '#70bff8'
+  } else if(hora >= 12 && hora <= 18) {
+    //BOA TARDE a56e45
     img.src = './images/foto-tarde.png'
+    document.body.style.background = '#a56e45'
   } else {
-    //BOA NOITE
+    //BOA NOITE 362a47
     img.src = './images/foto-noite.png'
+    document.body.style.background = '#362a47'
   }
 }
