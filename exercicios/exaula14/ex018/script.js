7cfe55991973f71cfe2b8a5ff593d1556ecf7aa3
Mateus Sgubim <101438688+mateussgubim@users.noreply.github.com>
2023-01-03 10:16:51 -0300

    Update script.js

--- exercicios/exaula14/ex018/script.js
+++ exercicios/exaula14/ex018/script.js
@@ -1,21 +1,3 @@
-function carregar() {
-  var msg = document.querySelector('#msg')
-  var img = document.querySelector('#imagem')
-  var data = new Date()
-  var hora = data.getHours()
-  msg.innerHTML = `Agora são ${hora} horas.`
-
-  if (hora >= 0 && hora < 12){
-    //BOM DIA 70bff8
-    img.src = './images/foto-manha.png'
-    document.body.style.background = '#70bff8'
-  } else if(hora >= 12 && hora <= 18) {
-    //BOA TARDE a56e45
-    img.src = './images/foto-tarde.png'
-    document.body.style.background = '#a56e45'
-  } else {
-    //BOA NOITE 362a47
-    img.src = './images/foto-noite.png'
-    document.body.style.background = '#362a47'
-  }
+function contar() {
+  
 }
