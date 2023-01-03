a3558fecac72f783b8f142f28e9283946c249e18
Mateus Sgubim <101438688+mateussgubim@users.noreply.github.com>
2023-01-03 09:53:50 -0300

    Update script.js

--- exercicios/exaula14/ex017/script.js
+++ exercicios/exaula14/ex017/script.js
@@ -17,13 +17,18 @@ function contar(){
   let res = document.querySelector('#res')
 
   if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-    window.alert('[ERRO] Faltam dados!')
     res.innerHTML = 'Impossível contar!'
+    //alert('[ERRO] Faltam dados!')
   } else {
-    res.innerHTML = 'Contando: '
+    res.innerHTML = 'Contando: <br>'
     let i = Number(ini.value)
     let f = Number(fim.value)
     let p = Number(passo.value)
+    if (p <= 0) {
+      alert('Passo inválido! Considerando passo 1')
+      p = 1
+    }
+
     if (i < f) {
       //Contagem crescente
       for (c = i; c <= f; c += p){
