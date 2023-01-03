103b0932d665855553adb6d17f64992d539f6458
Mateus Sgubim <101438688+mateussgubim@users.noreply.github.com>
2023-01-03 11:43:17 -0300

    .

--- exercicios/exaula14/ex018/script.js
+++ exercicios/exaula14/ex018/script.js
@@ -1,3 +1,18 @@
-function contar() {
-  
+function contar(){
+  let num = document.querySelector('#txtn')
+  let tab = document.querySelector('#seltab')
+  if(num.value.length == 0) {
+    alert('Digite um número!')
+  } else {
+    let n1 = Number(num.value)
+    let c = 1
+    tab.innerHTML = ''
+    while (c <= 10) {
+      let item = document.createElement('option')
+      item.text = `${n1} x ${c} = ${n1 * c}`
+      item.value = `tab${c}`
+      tab.appendChild(item)
+      c++
+    }
+  }
 }
