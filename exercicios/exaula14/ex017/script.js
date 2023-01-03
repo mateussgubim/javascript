f3a2cbfee7447df03212d60d9130a1e278e7cd90
Mateus Sgubim <101438688+mateussgubim@users.noreply.github.com>
2023-01-03 09:47:54 -0300

    Update script.js

--- exercicios/exaula14/ex017/script.js
+++ exercicios/exaula14/ex017/script.js
@@ -1,11 +1,39 @@
-function contar() {
-  var num1 = Number(document.querySelector('#num1'))
-  var num2 = Number(document.querySelector('#num2'))
-  var num3 = Number(document.querySelector('#num3'))
+function contar1() {
+  var num1 = Number(document.querySelector('#num1').value)
+  var num2 = Number(document.querySelector('#num2').value)
+  var num3 = Number(document.querySelector('#num3').value)
   var res = document.querySelector('#res')
 
   for (c = num1; c <= num2; c = c + num3) {
     res.innerHTML = 'Contando...'
     res.innerHTML += c
+  }
+}
+
+function contar(){
+  let ini = document.querySelector('#txti')
+  let fim = document.querySelector('#txtf')
+  let passo = document.querySelector('#txtp')
+  let res = document.querySelector('#res')
+
+  if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    window.alert('[ERRO] Faltam dados!')
+  } else {
+    res.innerHTML = 'Contando: '
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+    if (i < f) {
+      //Contagem crescente
+      for (c = i; c <= f; c += p){
+        res.innerHTML += ` ${c} \u{1F449}`
+      }
+      //Contagem regressiva
+    } else {
+      for (c = i; c >= f; c -= p) {
+        res.innerHTML += `${c} \u{1F449}`
+      }
+    }
+    res.innerHTML += `${c} \u{1F3C1}`
   }
 }
