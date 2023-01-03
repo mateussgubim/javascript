f56bd570d69cf659d27f6d7177afe6bf6e81a08d
Mateus Sgubim <101438688+mateussgubim@users.noreply.github.com>
2023-01-02 21:10:01 -0300

    Update script.js

--- exercicios/exaula14/ex017/script.js
+++ exercicios/exaula14/ex017/script.js
@@ -1,3 +1,11 @@
 function contar() {
-  
+  var num1 = Number(document.querySelector('#num1'))
+  var num2 = Number(document.querySelector('#num2'))
+  var num3 = Number(document.querySelector('#num3'))
+  var res = document.querySelector('#res')
+
+  for (c = num1; c <= num2; c = c + num3) {
+    res.innerHTML = 'Contando...'
+    res.innerHTML += c
+  }
 }
