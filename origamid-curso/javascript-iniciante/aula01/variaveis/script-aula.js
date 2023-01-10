3dd38361caa5e24388cfaeaaed4aa6f580f0652f
Mateus Sgubim <101438688+mateussgubim@users.noreply.github.com>
2023-01-10 15:10:05 -0300

    Create script-aula.js

---
+++ origamid-curso/javascript-iniciante/aula01/variaveis/script-aula.js
@@ -0,0 +1,14 @@
+var nome = 'Mateus';
+var idade = 21
+var possuiFaculdade = true
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+console.log(totalPreco);
+
+var estado = 'Paraná',
+    cidade = 'Mandaguari';
+console.log(nome, idade, possuiFaculdade, estado, cidade);
