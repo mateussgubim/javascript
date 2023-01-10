eaa0ec9182ff027017b438e14d6c45008073fdfe
Mateus Sgubim <101438688+mateussgubim@users.noreply.github.com>
2023-01-10 15:52:17 -0300

    Create script.js

---
+++ origamid-curso/javascript-iniciante/aula02/tipos-de-dados/script.js
@@ -0,0 +1,23 @@
+// Declare uma variável contendo uma string
+var texto = 'Uma string.';
+
+
+// Declare uma variável contendo um número dentro de uma string
+var numeroDentro = '2023';
+
+
+// Declare uma variável com a sua idade
+var idade = 21;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Mateus',
+    sobrenome = 'Sgubim';
+
+var nomeCompleto = `${nome} ${sobrenome}`;
+
+// Coloque a seguinte frase em uma variável: It's time
+texto = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
