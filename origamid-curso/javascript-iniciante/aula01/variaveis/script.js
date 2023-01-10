6fdcc2761d0aeb4b45f3aaf349792f0572c08834
Mateus Sgubim <101438688+mateussgubim@users.noreply.github.com>
2023-01-10 15:10:08 -0300

    Create script.js

---
+++ origamid-curso/javascript-iniciante/aula01/variaveis/script.js
@@ -0,0 +1,22 @@
+//Exercícios
+// Declarar uma variável com o seu nome
+var nome ='Mateus';
+
+// Declarar uma variável com a sua idade
+var idade = 21;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Pizza";
+
+// Declarar 5 variáveis diferentes sem valores
+var pais, 
+    estado,
+    cidade,
+    bairro,
+    rua;
+
+console.log(nome, idade, comidaFavorita);
