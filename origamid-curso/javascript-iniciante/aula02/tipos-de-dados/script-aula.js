8807979e024aab121b8bb783f6f4bb02aa1cae52
Mateus Sgubim <101438688+mateussgubim@users.noreply.github.com>
2023-01-10 15:52:15 -0300

    Create script-aula.js

---
+++ origamid-curso/javascript-iniciante/aula02/tipos-de-dados/script-aula.js
@@ -0,0 +1,21 @@
+var idade = 21;
+var time = null;
+var simbolo = Symbol();
+console.log(typeof simbolo);
+
+var nome = 'Mateus';
+var sobrenome = 'Sgubim';
+var nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto);
+
+// var gols = 1000;
+// var frase = 'Romário fez ' + gols + ' gols.'
+// console.log(typeof frase);
+
+var ano = '2023';
+var mes = 1;
+console.log(ano + mes);
+
+var gols = 1000;
+var frase = `Romário fez ${gols} gols.`
+console.log(frase);
